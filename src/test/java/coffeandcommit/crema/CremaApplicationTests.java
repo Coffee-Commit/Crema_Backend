f2fa8b1d4f900304081d82ea7b42a52538d02527
Roysung0625 <roy06255@gmail.com>
2025-08-13 14:17:33 +0900

    init

---
+++ src/test/java/coffeandcommit/crema/CremaApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeandcommit.crema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CremaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
