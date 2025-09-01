dd0e3643bb41baadd5365a08ae70ea33ed8cd4b8
Yunyoung Jang <n0stress@naver.com>
2025-09-02 08:11:01 +0900

    GuideChatTopicRequestDTO 유효성 검증 어노테이션 추가
    - `@Valid` 추가로 내부 객체 유효성 검증 강화
    - 불필요한 import 제거

--- src/main/java/coffeandcommit/crema/domain/guide/dto/request/GuideChatTopicRequestDTO.java
+++ src/main/java/coffeandcommit/crema/domain/guide/dto/request/GuideChatTopicRequestDTO.java
@@ -1,11 +1,13 @@
 package coffeandcommit.crema.domain.guide.dto.request;
 
 import coffeandcommit.crema.domain.globalTag.dto.TopicDTO;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ import java.util.List;
 @Builder
 public class GuideChatTopicRequestDTO {
 
+    @Valid
     @NotNull
     private List<TopicDTO> topics;
 }
