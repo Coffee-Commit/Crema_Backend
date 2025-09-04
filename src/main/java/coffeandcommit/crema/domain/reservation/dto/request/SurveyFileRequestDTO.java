479ee1d0a545dbd14a4b378ea14b37748e8b3de7
Yunyoung Jang <n0stress@naver.com>
2025-09-05 07:54:26 +0900

    Update - 불필요한 import 제거: SurveyFileRequestDTO 및 MemberRepository

--- src/main/java/coffeandcommit/crema/domain/reservation/dto/request/SurveyFileRequestDTO.java
+++ src/main/java/coffeandcommit/crema/domain/reservation/dto/request/SurveyFileRequestDTO.java
@@ -1,7 +1,6 @@
 package coffeandcommit.crema.domain.reservation.dto.request;
 
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
