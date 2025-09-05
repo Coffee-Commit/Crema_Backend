f9809a06ed92aa76eff3cef9262380e767803fa9
Yunyoung Jang <n0stress@naver.com>
2025-09-06 08:37:27 +0900

    Update - GuideRepository에서 @EntityGraph 제거

--- src/main/java/coffeandcommit/crema/domain/guide/repository/GuideRepository.java
+++ src/main/java/coffeandcommit/crema/domain/guide/repository/GuideRepository.java
@@ -1,7 +1,6 @@
 package coffeandcommit.crema.domain.guide.repository;
 
 import coffeandcommit.crema.domain.guide.entity.Guide;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +9,6 @@ import java.util.Optional;
 @Repository
 public interface GuideRepository extends JpaRepository<Guide, Long> {
 
-    @EntityGraph(attributePaths = {"member"})
-    Optional<Guide> findById(Long id);
 
     Optional<Guide> findByMember_Id(String memberId);
 }
