d7e00e2792c53938f8f42d91c68647ceefba1970
csy <tnehd83@naver.com>
2025-09-05 23:33:38 +0900

    test: storage 관련 단위 테스트, 통합 테스트 추가

---
+++ src/test/java/coffeandcommit/crema/global/storage/integration/StorageServiceIntegrationTest.java
@@ -0,0 +1,75 @@
+package coffeandcommit.crema.global.storage.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+
+import coffeandcommit.crema.global.file.FileService;
+import coffeandcommit.crema.global.storage.StorageService;
+import coffeandcommit.crema.global.storage.dto.FileUploadResponse;
+import coffeandcommit.crema.global.validation.FileType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+@SpringBootTest
+public class StorageServiceIntegrationTest {
+
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private StorageService storageService;
+
+    @Value("${spring.cloud.gcp.storage.bucket-name}")
+    private String bucketName;
+
+    private String uploadedFileKey = null;
+
+
+    @AfterEach
+    void tearDown() {
+        if (uploadedFileKey != null) {
+            System.out.println("Clean up file : " + uploadedFileKey);
+            storageService.deleteFile(uploadedFileKey);
+            uploadedFileKey = null;
+        }
+    }
+
+    @Test
+    @DisplayName("GCS 파일 업로드 및 URL 조회 후 삭제")
+    void uploadAndViewAndDelete_IntegrationTest() {
+        // given
+        MockMultipartFile testFile = new MockMultipartFile(
+                "file",
+                "integration-test-file.png",
+                "image/png",
+                "This is an integration test.".getBytes()
+        );
+
+        FileUploadResponse response = fileService.uploadFile(
+                testFile,
+                FileType.IMAGE,
+                "integration-tests",
+                "test-user"
+        );
+
+        // then
+        assertNotNull(response);
+        assertNotNull(response.getFileKey());
+        assertThat(response.getFileUrl()).contains(bucketName);
+
+        // given
+        this.uploadedFileKey = response.getFileKey();
+
+        // when
+        String viewUrl = storageService.generateViewUrl(this.uploadedFileKey);
+
+        System.out.println("Generated View URL: " + viewUrl);
+        assertThat(viewUrl).isNotNull();
+        assertThat(viewUrl).contains("storage.googleapis.com");
+    }
+}
