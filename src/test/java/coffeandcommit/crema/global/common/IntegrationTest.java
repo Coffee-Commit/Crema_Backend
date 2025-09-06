bbe7c5022ce7b0587519c2205d405fb7f232a44b
csy <tnehd83@naver.com>
2025-09-07 00:03:46 +0900

    fix:test last

--- src/test/java/coffeandcommit/crema/global/common/IntegrationTest.java
+++ src/test/java/coffeandcommit/crema/global/common/IntegrationTest.java
@@ -7,11 +7,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password="
+})
 @Transactional
 public @interface IntegrationTest {
 }
