14f7a642f83a63ee1a320e9d008516ab683a08c8
Roysung0625 <roy06255@gmail.com>
2025-09-09 23:27:03 +0900

    fix: TopicNameType enum 생성자 및 getter 메서드 추가
    - @AllArgsConstructor 대신 명시적 생성자 정의
    - getDescription() 메서드 명시적 구현
    - String 파라미터를 받는 생성자 컴파일 에러 해결

--- src/main/java/coffeandcommit/crema/domain/globalTag/enums/TopicNameType.java
+++ src/main/java/coffeandcommit/crema/domain/globalTag/enums/TopicNameType.java
@@ -1,11 +1,9 @@
 package coffeandcommit.crema.domain.globalTag.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@AllArgsConstructor
 @ToString
 public enum TopicNameType {
     UNDEFINED("미정"),
@@ -24,4 +22,12 @@ public enum TopicNameType {
     JOB_CHANGE("이직");
 
     private final String description;
+    
+    TopicNameType(String description) {
+        this.description = description;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
 }
